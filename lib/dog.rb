6e411c9e5b8dad576178cc6941da16589f85d304
github username <github email address>
2019-08-19 10:01:41 -0700

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,2 +1,17 @@
-class Dog
+class Dog < ActiveRecord::Base
+    # attr_reader :name, :breed
+    # def initialize(input={})
+    #     @name = input[:name]
+    #     @breed = input[:breed]
+    #     @db = DB
+    # end
+
+    # def self.create(input)
+    #     sql = "INSERT INTO dogs(name, breed) VALUES (?, ?)"  
+    #     @db.execute(sql, input[:name], input[:breed])
+    # end
+
+    # # def 
+    # # end 
+
 end
